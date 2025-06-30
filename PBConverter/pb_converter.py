3930dd3b8f4b264ba65713b00ed0686d9fe7d901
Ricky Cheng <rcheng@col.org>
2025-06-30 16:44:15 -0700

    "Resource Management in Rainfed Drylands" book updated with missing PDF files

--- PBConverter/pb_converter.py
+++ PBConverter/pb_converter.py
@@ -414,12 +414,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert a Pressbooks site to a static version")
     parser.add_argument(
         "--url",
-        default="https://opentextbooks.colvee.org/animalnutritionforlivestockandpoultryproductivity/",
+        default="https://opentextbooks.colvee.org/resourcemanagementrainfeddrylands/",
         help="Source Pressbooks URL",
     )
     parser.add_argument(
         "--output",
-        default="animalnutritionforlivestockandpoultryproductivity-pdf",
+        default="resourcemanagementrainfeddrylands-pdf",
         help="Output directory",
     )
 

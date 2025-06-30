8ba61629526b3a8e3ad66bd8f91fda35783cd13c
Ricky Cheng <rcheng@col.org>
2025-06-30 16:57:15 -0700

    "Detection, Diagnosis and Management of Plant Diseases" book updated with missing PDF files

--- PBConverter/pb_converter.py
+++ PBConverter/pb_converter.py
@@ -414,12 +414,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert a Pressbooks site to a static version")
     parser.add_argument(
         "--url",
-        default="https://opentextbooks.colvee.org/valueengineeringagriculturalplan/",
+        default="https://opentextbooks.colvee.org/managementofplantdiseases/",
         help="Source Pressbooks URL",
     )
     parser.add_argument(
         "--output",
-        default="valueengineeringagriculturalplan-pdf",
+        default="managementofplantdiseases-pdf",
         help="Output directory",
     )
 
